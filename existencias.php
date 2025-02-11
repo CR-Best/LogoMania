8bb3ed87f7f5f41d68948861c9cc7d59bf20ac35
CR-Best <crosales3103@gmail.com>
2025-02-11 11:03:44 -0600

    se actualizo el codigo

--- existencias.php
+++ existencias.php
@@ -1,62 +1,50 @@
-<?php 
-include("plus/conexion.lm");
-
-
-include("plus/header.lm");
-?>
 <?php
-
-include("plus/top.lm");
-
-
+session_start();
+require_once "db.php";
+include "plus/header.lm";
 ?>
+
+<body>
+<?php include "plus/top.lm"; ?>
+
 <table width="90%" border="3" align="center" cellpadding="4" cellspacing="4" bordercolor="#164E7F">
-              
-		   <?php
-	$sql = "SELECT * FROM material"; 
-	$buscar=mysql_query($sql,$conectar)or die("error en consulta");
-	$total=mysql_num_rows($buscar);
-	echo "<tr><td colspan=5 class=titulo>Registros encontrados: <b>$total</b></td></tr>";
-		   ?>
-          <tr>
-            <td class=titulo><b>Nombre del Material</b></td>
-            <td align="center" class=titulo>Costo Promedio</td>
-            <td align="center" class=titulo>Existencias</td>
-          </tr>
-		  <?php
-		  
-		  
+    <?php
+    $sql = "
+        SELECT m.idmaterial, m.nombrematerial, m.medidamaterial, 
+               IFNULL(i.cantidad_material, 0) AS existencias, 
+               IFNULL(i.costomaterial, 0) AS costo
+        FROM material m
+        LEFT JOIN inventario i ON m.idmaterial = i.idmaterial
+    ";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $total = $result->num_rows;
 
+    echo "<tr><td colspan=3 class='titulo'>Registros encontrados: <b>$total</b></td></tr>";
+    ?>
+    <tr>
+        <td class="titulo"><b>Nombre del Material</b></td>
+        <td align="center" class="titulo">Costo Promedio</td>
+        <td align="center" class="titulo">Existencias</td>
+    </tr>
+    <?php while ($res = $result->fetch_assoc()): ?>
+        <tr>
+            <td>
+                <?php if ($res["existencias"] > 0): ?>
+                    <a href="invsalida.php?idmaterial=<?php echo $res['idmaterial']; ?>">
+                        <?php echo htmlspecialchars($res["nombrematerial"]); ?>
+                    </a>
+                <?php else: ?>
+                    <?php echo htmlspecialchars($res["nombrematerial"]); ?>
+                <?php endif; ?>
+            </td>
+            <td align="center">$ <?php echo number_format($res["costo"], 2, '.', ''); ?></td>
+            <td align="center"><?php echo $res["existencias"] . " " . htmlspecialchars($res["medidamaterial"]); ?></td>
+        </tr>
+    <?php endwhile; ?>
+</table>
 
-		  
-		  while($res=mysql_fetch_array($buscar))
-		  {
-			$bus="SELECT * FROM inventario WHERE idmaterial=\"$res[0]\"";
-			$consulta=mysql_query($bus, $conectar) or die("Error");
-			if(mysql_num_rows($consulta)>=1)
-				$row = mysql_fetch_row($consulta);
-			else
-			{
-				$row[1]=0;
-				$row[2]=0;
-			
-			}
-		  echo "<tr>
-            <td>";
-			if($row[1]>0)
-				echo "<a href=invsalida.php?idmaterial=$res[0]>$res[1]</a>";
-			else
-				echo "$res[1]";
-
-			echo"</td>
-            <td align=center>$ ".number_format((($row[2]*100)/100), 2, '.', '')."</a></td>
-            <td align=center>$row[1] $res[medidamaterial]</td>
-			
-          </tr>";
-		  }
-		  echo "</table>";
-?>
-<?php 
-	  include("plus/bottom.lm");
-	  ?>
-	
+<?php include "plus/bottom.lm"; ?>
+</body>
+</html>
